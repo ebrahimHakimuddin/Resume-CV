3df69b43aaf37d3e381a5409e26e75ebbcd14c07
Ebrahim Tayabali <ebrahimhakimuddin@gmail.com>
2024-09-13 00:03:55 +0300

    Added Education & Work Experince

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    screens: {
+      'tablet': '620px',
+      'laptop': '1200px',
+      'large':'1440px'
+    },
+    fontSize:{
+      xs:'0.5rem',
+      sm: '0.6rem',
+      'smbase':'0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    extend: {
+      colors:{
+        'darkmode' : '#121212'
+      }
+    },
   },
   plugins: [],
 }
